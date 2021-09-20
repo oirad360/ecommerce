2d3f65ed438defd2d60ea61c562bba72be33b978
Dario Anzalone <oirad36043@gmail.com>
2021-09-20 16:25:47 +0200

    Rimossi font inutili

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -6,11 +6,11 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet"> -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ $csrf_token }}">
         <script src='/{{$app_folder}}/public/scripts/env.js' defer></script>
@@ -41,40 +41,40 @@
                 </form>
                 <div class="searchResults hidden" ></div>
             </div>
-            <nav>
-                <div id=navMain>
-                    <div class=buttonContainer>
-                        @if(isset($username))
-                            <div class=profileContainer>
-                                <span>Benvenuto, {{$username}}</span>
-                                <a class=navButton href="/{{$app_folder}}/public/seller/{{$username}}">Visita la tua pagina</a>
-                            </div>
-                            <a href="/{{$app_folder}}/public/cart" id=cart>
-                                <span>{{$cartItems}}</span>
-                            </a>
-                        @else
-                            <a href="/{{$app_folder}}/public/signup" class=navButton id=signupButton>Registrati</a>
-                            <a href="/{{$app_folder}}/public/login" class=navButton>Accedi</a>
-                        @endif
-                    </div>
-                    
-                    <a href=/{{$app_folder}}/public/home>
-                        <img src=/{{$app_folder}}/public/assets/home.png id=homeButton />
-                    </a>
-                </div>
-                <div id=navCategories>
-                    <a href="/{{$app_folder}}/public/search?c=smartphone" data-value=smartphone>Smartphone</a>
-                    <a href="/{{$app_folder}}/public/search?c=laptop" data-value=laptop>Laptop</a>
-                    <a href="/{{$app_folder}}/public/search?c=pc" data-value=pc>PC</a>
-                    <a href="/{{$app_folder}}/public/search?c=audio" data-value=audio>Audio</a>
-                    <a href="/{{$app_folder}}/public/search?c=tv" data-value=tv>TV</a>
-                    <a href="/{{$app_folder}}/public/search?c=photography" data-value=photography>Fotografia</a>
-                    <a href="/{{$app_folder}}/public/search?c=console" data-value=console>Console</a>
-                    <a href="/{{$app_folder}}/public/search?c=smartwatch" data-value=smartwatch>Smartwatch</a>
-                    <a href="/{{$app_folder}}/public/search?c=accessories" data-value=accessories>Accessori</a>
-                </div>
-            </nav>
         </header>
+        <nav>
+            <div id=navMain>
+                <div class=buttonContainer>
+                    @if(isset($username))
+                        <div class=profileContainer>
+                            <span>Benvenuto, {{$username}}</span>
+                            <a class=navButton href="/{{$app_folder}}/public/seller/{{$username}}">Visita la tua pagina</a>
+                        </div>
+                        <a href="/{{$app_folder}}/public/cart" id=cart>
+                            <span>{{$cartItems}}</span>
+                        </a>
+                    @else
+                        <a href="/{{$app_folder}}/public/signup" class=navButton id=signupButton>Registrati</a>
+                        <a href="/{{$app_folder}}/public/login" class=navButton>Accedi</a>
+                    @endif
+                </div>
+                
+                <a href=/{{$app_folder}}/public/home>
+                    <img src=/{{$app_folder}}/public/assets/home.png id=homeButton />
+                </a>
+            </div>
+            <div id=navCategories>
+                <a href="/{{$app_folder}}/public/search?c=smartphone" data-value=smartphone>Smartphone</a>
+                <a href="/{{$app_folder}}/public/search?c=laptop" data-value=laptop>Laptop</a>
+                <a href="/{{$app_folder}}/public/search?c=pc" data-value=pc>PC</a>
+                <a href="/{{$app_folder}}/public/search?c=audio" data-value=audio>Audio</a>
+                <a href="/{{$app_folder}}/public/search?c=tv" data-value=tv>TV</a>
+                <a href="/{{$app_folder}}/public/search?c=photography" data-value=photography>Fotografia</a>
+                <a href="/{{$app_folder}}/public/search?c=console" data-value=console>Console</a>
+                <a href="/{{$app_folder}}/public/search?c=smartwatch" data-value=smartwatch>Smartwatch</a>
+                <a href="/{{$app_folder}}/public/search?c=accessories" data-value=accessories>Accessori</a>
+            </div>
+        </nav>
             
         @yield('section')
 
